14ce8dabe5e6d397c0cf99216f52030cf889d900
Jonathan Provost <jprovost.sh@gmail.com>
2020-09-02 16:59:50 -0400

    Hide system cursor when using the mouse as a joystick
    It was confusing to see the system cursor AND the cursor rendered
    by the application running in the emulator, especially when the
    joystick range is not fully used an the offset between the two cursor
    is visible.

--- src/com/joprovost/r8bemu/io/awt/MouseJoystickDriver.java
+++ src/com/joprovost/r8bemu/io/awt/MouseJoystickDriver.java
@@ -4,11 +4,16 @@ import com.joprovost.r8bemu.data.BitOutput;
 import com.joprovost.r8bemu.io.Joystick;
 
 import javax.swing.event.MouseInputAdapter;
+import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 public class MouseJoystickDriver extends MouseInputAdapter {
     private final Joystick joystick;
     private final BitOutput mouse;
+    private final Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+            new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB),
+            new Point(0, 0), "blank cursor");
 
     public MouseJoystickDriver(Joystick joystick, BitOutput mouse) {
         this.joystick = joystick;
@@ -17,34 +22,41 @@ public class MouseJoystickDriver extends MouseInputAdapter {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        if (mouse.isClear()) return;
-        move(e);
-        joystick.press();
+        press(e);
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        if (mouse.isClear()) return;
-        move(e);
-        joystick.release();
+        release(e);
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        if (mouse.isClear()) return;
-        move(e);
-        joystick.release();
+        release(e);
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        if (mouse.isClear()) return;
+        press(e);
+    }
+
+    private void press(MouseEvent e) {
         move(e);
-        joystick.press();
+        if (mouse.isSet()) joystick.press();
+    }
+
+    private void release(MouseEvent e) {
+        move(e);
+        if (mouse.isSet()) joystick.release();
     }
 
     private void move(MouseEvent e) {
-        joystick.horizontal(e.getX() * (Joystick.MAXIMUM - Joystick.MINIMUM) / e.getComponent().getWidth() + Joystick.MINIMUM);
-        joystick.vertical(e.getY() * (Joystick.MAXIMUM - Joystick.MINIMUM) / e.getComponent().getHeight() + Joystick.MINIMUM);
+        if (mouse.isSet()) {
+            joystick.horizontal(e.getX() * (Joystick.MAXIMUM - Joystick.MINIMUM) / e.getComponent().getWidth() + Joystick.MINIMUM);
+            joystick.vertical(e.getY() * (Joystick.MAXIMUM - Joystick.MINIMUM) / e.getComponent().getHeight() + Joystick.MINIMUM);
+            e.getComponent().setCursor(blankCursor);
+        } else {
+            e.getComponent().setCursor(Cursor.getDefaultCursor());
+        }
     }
 }
